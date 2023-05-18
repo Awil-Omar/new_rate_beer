813a2f8d0cdca9d0f66ec56d54df3281713ae18e
javaman-22 <daroor9@gmail.com>
2023-05-18 11:38:58 +0200

    added new.html.erb to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+    def new
+      # render the signing up page
+    end
+
+    def create
+      # retrieves from the database the user that matches the username
+      user = User.find_by username: params[:username]
+      # saves the user ID who signed up (if the user exists)
+      session[:user_id] = user.id unless user.nil?
+      #regirects the user to their own page
+      redirect_to user
+    end
+
+    def destroy
+      # resets the session
+      session[:user_id] = nil
+      # redericts the application to the main page
+      redirect_to :root
+    end
 end
